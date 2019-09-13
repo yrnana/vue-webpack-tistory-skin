d749d9e74495193a3409e7e5e2396a8ed8419210
YuriNa <nyryn0945@gmail.com>
2019-09-13 16:12:27 +0900

    1차마무리

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ import './plugins/antd'
 // ignore tistory tag
 Vue.config.ignoredElements = [/^s_/]
 
-const vm = new Vue({
+new Vue({
 	el: '#app',
 	data() {
 		return {
 			open: window.matchMedia('(min-width: 992px)').matches,
-			name: '',
-			password: '',
-			secret: false,
-			comment: '',
 		}
 	},
 	methods: {
@@ -29,106 +25,11 @@ const vm = new Vue({
 			}
 			window.location.href = '/search/' + looseURIEncode(value)
 		},
-		addComment(callback) {
+		reloadEntry(callback) {
 			var pattern = /([1-9][0-9]*)/
 			var id = callback.match(pattern)[0]
-			addComment(this.$refs.addCmtBtn.$el, id)
+			reloadEntry(Number(id))
 			return false
 		},
 	},
 })
-
-/**
- * 티스토리 함수
- *
- * @param {*} caller
- * @param {*} entryId
- */
-function addComment2(caller, entryId) {
-	;(function($) {
-		var oForm = findFormObject(caller)
-		if (!oForm) {
-			return false
-		}
-
-		var data = {
-			key: 'tistory',
-		}
-
-		var $captchaInput = $('#inputCaptcha')
-		if ($captchaInput.length > 0) {
-			if (!$captchaInput.val()) {
-				alert('그림문자를 입력해 주세요.')
-				return false
-			}
-
-			data.captcha = $captchaInput.val()
-		}
-
-		if (oForm['name']) {
-			data.name = oForm['name'].value
-		}
-
-		if (oForm['password']) {
-			var passwd = oForm['password'].value.trim()
-			if (passwd.length == 0) {
-				alert('비밀번호를 입력해 주세요.')
-				return false
-			}
-
-			var shaObj = new jsSHA('SHA-256', 'TEXT')
-			shaObj.update(md5(encodeURIComponent(passwd)))
-			data.password = shaObj.getHash('HEX')
-		}
-
-		if (oForm['homepage']) {
-			data.homepage = oForm['homepage'].value
-		}
-
-		if (oForm['secret'] && oForm['secret'].checked) {
-			data.secret = 1
-		}
-
-		if (oForm['comment']) {
-			data.comment = oForm['comment'].value
-		}
-
-		$.ajax({
-			url: oForm.action + '?__T__=' + new Date().getTime(),
-			method: 'post',
-			data: data,
-		})
-			.done(function(r) {
-				if (entryId == 0) {
-					window.location = blogURL + '/guestbook'
-					return
-				}
-
-				var data = r.data
-				var $comments = $('#entry' + entryId + 'Comment .commentList'),
-					$recentComments = $('#recentComments'),
-					$commentCountOnRecentEntries = $('#commentCountOnRecentEntries' + entryId)
-
-				$comments.html(data.commentBlock.split('/form>')[1])
-				$recentComments.html(data.recentCommentBlock)
-				for (var i = 0; i < data.commentViews.length; i++) {
-					$('#commentCount' + entryId + '_' + i).html(data.commentViews[i])
-				}
-				$commentCountOnRecentEntries.html('(' + data.commentCount + ')')
-
-				if (typeof window.needCommentCaptcha !== 'undefined') {
-					captchaPlugin.init('complete')
-				}
-
-				// vm.name = ''
-				// vm.password = ''
-				vm.comment = ''
-				vm.secret = false
-			})
-			.fail(function(r) {
-				alert(r.responseJSON.data)
-			})
-	})(tjQuery)
-}
-
-window.addComment = addComment2
